27edcdce1bfe5c6ecc7907aa04d019c6ff2c2c9b
jianxhe <aumashejianxin@gmail.com>
2016-10-03 15:03:14 -0500

    commit test

--- gen/org/esteban/piano/BuildConfig.java
+++ gen/org/esteban/piano/BuildConfig.java
@@ -1,6 +1,7 @@
 /** Automatically generated file. DO NOT MODIFY */
 // this is a github test
 //test 1 ttsafdfasdf a fasdf sfa fasd adfa fda fasdf asdf a
+// asf asdf asdfa fasdf
 package org.esteban.piano;
 
 public final class BuildConfig {
