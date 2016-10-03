518cebcdd7014aaba6bbcd83e586cd3121895f80
He <jianxhe@uiowa.edu>
2016-10-03 14:39:28 -0500

    Initial commit

---
+++ gen/org/esteban/piano/BuildConfig.java
@@ -0,0 +1,8 @@
+/** Automatically generated file. DO NOT MODIFY */
+// this is a github test
+//test 1
+package org.esteban.piano;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
